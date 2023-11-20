237a8ac35dadb0b25a2061b3c34e0fb033bc0217
Gerson Azabache <gerson_dunk@hotmail.com>
2023-11-20 10:42:46 -0500

    classes and methods added

---
+++ InstanceStaticMethods/Business/ClienteGestion.cs
@@ -0,0 +1,52 @@
+﻿using InstanceStaticMethods.Entities;
+using System.Runtime.CompilerServices;
+
+namespace InstanceStaticMethods.Business
+{
+    public class ClienteGestion
+    {
+        //Propiedades
+        public Cliente Cliente { get; set; }
+        public int AniosAntiguedad { get; set; }
+        public double MontoComprasAcumuladas { get; set; }
+        public double MontoDeudaActual { get; set; }
+
+        public static double TotalIngresos;
+
+        //Métodos de instancia
+        public string obtenerStatus()
+        {
+            //Lógica del negocio
+            var response = "";
+
+            if (AniosAntiguedad > 1)
+            {
+                if (MontoDeudaActual == 0)
+                {
+                    response = MontoComprasAcumuladas > 1000 ? "Cliente premium" : "Cliente preferencial";
+                }
+                else
+                {
+                    response = MontoDeudaActual / MontoComprasAcumuladas < 0.25 ? "Cliente comun" : "Cliente riesgoso";
+                }
+            }
+            else
+            {
+                response = "Cliente sin historial suficiente";
+            }
+
+            return response;
+        }
+
+        //Métodos estáticos
+        public static void CalculateTax()
+        {            
+            Console.WriteLine($"Tus impuestos son: {TotalIngresos * 0.2}");
+        }
+
+        public static void CalculateTax(double totalIngresos)
+        {
+            Console.WriteLine($"Tus impuestos son: {totalIngresos * 0.2}");
+        }
+    }
+}
---
+++ InstanceStaticMethods/Program.cs
@@ -0,0 +1,23 @@
+﻿using InstanceStaticMethods.Business;
+using InstanceStaticMethods.Entities;
+
+Console.WriteLine("Métodos de instancia vs métodos estáticos");
+
+/****** Método de instancia ******/
+Cliente cliente = new Cliente();
+cliente.Id = 1;
+cliente.Nombre = "Juan Valdez";
+
+ClienteGestion objetoGestion = new ClienteGestion();
+objetoGestion.Cliente = cliente;
+objetoGestion.AniosAntiguedad = 2;
+objetoGestion.MontoComprasAcumuladas = 1001;
+objetoGestion.MontoDeudaActual = 0;
+
+Console.WriteLine(objetoGestion.obtenerStatus());
+
+/****** Métodos estáticos ******/
+ClienteGestion.TotalIngresos = 3000;
+ClienteGestion.CalculateTax();
+
+ClienteGestion.CalculateTax(2500);
